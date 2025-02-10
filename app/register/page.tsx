dcd2ff0b8f2bca4514d79fec8133cbcd3496ecb6
Otto Oikarinen <80541689+OttoOikarinen@users.noreply.github.com>
2025-02-10 19:25:15 +0200

    Added the UI needed for registering.

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,7 +1,16 @@
-export default function Register() {
-    return (
-        <div>
-            Register page
-        </div>
-    )
+// I have used the nextjs-dashboard tutorial a source for the file, mainly because of the styling. There isn't any functionality. 
+
+import RegisterForm from '@/app/ui/register-form';
+import { Suspense } from 'react';
+ 
+export default function RegisterPage() {
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <Suspense>
+          <RegisterForm />
+        </Suspense>
+      </div>
+    </main>
+  );
 }
