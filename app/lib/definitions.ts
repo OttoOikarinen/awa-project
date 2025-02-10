b0dea77433a93c4cdaf318145b4687e1f1b17d4a
Otto Oikarinen <80541689+OttoOikarinen@users.noreply.github.com>
2025-02-10 19:22:11 +0200

    Created definitions for types used in the application.

---
+++ app/lib/definitions.ts
@@ -0,0 +1,21 @@
+export type User = {
+    id: string,
+    name: string,
+    email: string,
+    password: string
+}
+
+export type Column = {
+    id: string,
+    user_id: string,
+    column_name: string,
+    column_index: number,
+}
+
+export type Todo = {
+    id: string,
+    user_id: string,
+    column_id: string,
+    task: string,
+    todo_index: number
+}
