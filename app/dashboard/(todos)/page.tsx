ca904efb712b03eab8488d364429862b14fe51a7
Otto Oikarinen <80541689+OttoOikarinen@users.noreply.github.com>
2025-02-20 17:34:51 +0200

    Added buttons.

--- app/dashboard/(todos)/page.tsx
+++ app/dashboard/(todos)/page.tsx
@@ -1,9 +1,25 @@
 
 import ColumnWrapper from '@/app/ui/dashboard/columns';
+import { Button } from '@/app/ui/buttons';
+import Link from 'next/link';
 
 export default function Todos() {
     return (
         <div>
+            <div className="flex gap-4">
+                <Link
+                  href="/dashboard/create-todo"
+                  className="px-4 py-2 border border-gray-500 text-gray-900 rounded-lg hover:bg-gray-100 transition"
+                >
+                  Create Todo
+                </Link>
+                <Link
+                  href="/dashboard/create-column"
+                  className="px-4 py-2 border border-gray-500 text-gray-900 rounded-lg hover:bg-gray-100 transition"
+                >
+                  Create Column
+                </Link>
+            </div>
             <ColumnWrapper />
         </div>
     )
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       Home page
-      Login
-      Register
+      <div className="flex gap-4">
+        <Link
+          href="/register"
+          className="px-4 py-2 border border-gray-500 text-gray-900 rounded-lg hover:bg-gray-100 transition"
+        >
+          Register
+        </Link>
+        <Link
+          href="/login"
+          className="px-4 py-2 border border-gray-500 text-gray-900 rounded-lg hover:bg-gray-100 transition"
+        >
+          Login
+        </Link>
+            </div>
     </div>
   );
 }
