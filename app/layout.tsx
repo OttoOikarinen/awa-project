923829813867e832bf603f37c6a6d29ea028ec4e
Otto Oikarinen <80541689+OttoOikarinen@users.noreply.github.com>
2025-03-01 16:17:13 +0200

    Added metadata.

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,38 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+
 export const metadata: Metadata = {
-  title: "Todo App",
-  description: "Manage your todo list.",
+  title: 'Todo app',
+  description: 'Best way to handle your todos in kanban style boards.',
+  keywords: ['todos', 'kanban'],
+  openGraph: {
+    title: 'Todo app',
+    description: 'Best way to handle your todos in kanban style boards.',
+    url: '/',
+    siteName: 'Todo app',
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Todo app',
+    description: 'Best way to handle your todos in kanban style boards.',
+  },
+  icons: {
+    icon: '/favicon.ico',
+  },
+  robots: {
+    index: true,
+    follow: true,
+  },
+  viewport: 'width=device-width, initial-scale=1',
+  alternates: {
+    canonical: '/',
+  },
+  other: {
+    'google-site-verification': 'xyz789',
+  },
 };
 
 export default function RootLayout({
--- middleware.ts
+++ middleware.ts
@@ -37,5 +37,5 @@ export default async function middleware(req: NextRequest) {
  
 // Routes Middleware should not run on
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.ts|robots.ts|sitemap.xml|.*\\.png$).*)'],
 }
